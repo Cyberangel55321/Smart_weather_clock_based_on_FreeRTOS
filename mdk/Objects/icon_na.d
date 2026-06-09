1e79887fb4c1a92b7731578e1f4afdea888bfe5d
Cyberangel55321 <553212114@qq.com>
2026-06-10 00:03:20 +0800

    修改.gitignore文件

--- mdk/Objects/icon_na.d
+++
@@ -1,3 +0,0 @@
-.\objects\icon_na.o: ..\app\image\icon_na.c
-.\objects\icon_na.o: ..\app\image\image.h
-.\objects\icon_na.o: F:\APP\keil\keil5\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- mdk/Objects/img_wifi.d
+++
@@ -1,3 +0,0 @@
-.\objects\img_wifi.o: ..\app\image\img_wifi.c
-.\objects\img_wifi.o: ..\app\image\image.h
-.\objects\img_wifi.o: F:\APP\keil\keil5\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
